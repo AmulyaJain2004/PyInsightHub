5d48f4038e78f15b3300d7f39a62801aaa9b50fd
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-04 20:36:02 +0530

    Create Calculator.py
    Basic calculator to apply input two numbers doing arithmetic operations and output the results

---
+++ Python_Learners_Hub/Calculator.py
@@ -0,0 +1,12 @@
+print("Enter the First Number: -")
+number1 = int(input())
+print("Enter the Second Number: -")
+number2 = int(input())
+
+summ = a + b
+diff = a - b
+multiply = a*b
+
+print("The sum of Two numbers",summ)
+print("The difference of Two numbers",diff)
+print("The product of Two numbers",multiply)
