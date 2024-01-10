2dde167928566b331d3e98049ae2ee1ac2774950
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-10 19:11:04 +0530

    Create c_Different_Data_Types.py
    Yet to complete :) 
    Program about different data types in Python Programming Language.

---
+++ Python_Learners_Hub/c_Different_Data_Types.py
@@ -0,0 +1,9 @@
+m=6
+n=8.
+o=987.4579929090
+p=1j
+q=["apple","banana","cherry"]
+r=("apple","banana","cherry")
+s={"Name":"John"}
+t=True
+print(type(m),type(n),type(o),type(p),type(q),type(r),type(s),type(t))
