843dc6cd8ae40bd673a909149668c526f91c4ba0
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-11 13:35:14 +0530

    Create b_sum_natural_num.py
    Program to find sum of all natural numbers from 1 to the number given by the user.

---
+++ Python_Learners_Hub/d_Basic_Input_Output /b_sum_natural_num.py
@@ -0,0 +1,5 @@
+number =  int (input('Enter the number till which sum of all natural numbers has to be done: '))
+
+summation = (number*(number+1))//2 // Formula of Sum of Arithmetic Progression 
+
+print("Summation of numbers: ",summation)
