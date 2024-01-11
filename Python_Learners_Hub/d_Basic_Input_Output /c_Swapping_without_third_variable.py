02c0f737cd78063e49ff2713abb56fb892325840
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-11 13:37:42 +0530

    Create c_Swapping_without_third_variable.py
    Program to swap values of two variables given by user without using third variable.

---
+++ Python_Learners_Hub/d_Basic_Input_Output /c_Swapping_without_third_variable.py
@@ -0,0 +1,9 @@
+a = int (input("Enter the first number: "))
+b = int (input("Enter the second number: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After Swappping value of first number: ", a)
+print("After Swappping value of second number: ", b)
