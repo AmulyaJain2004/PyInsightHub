50f7222850c47c6066674ef88b25973ef67110ce
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-17 22:17:01 +0530

    Update and rename Check_Leap_year.py to a_Check_Leap_year.py
    Program to check whether a given year is leap year or not.

--- Python_Learners_Hub/e_Decision_Making /a_Check_Leap_year.py
+++ Python_Learners_Hub/e_Decision_Making /a_Check_Leap_year.py
@@ -1,12 +1,16 @@
 year = int(input("Enter year to check whether it is leap year or not: "))
 
 if (year%4==0):
+    
     if (year%100==0):
+        
         if (year%400==0):
             print("Given year is leap year")
         else:
             print("Given year is not leap year")
+            
     else:
         print("Given year is leap year")
+        
 else:
     print("Given year is not leap year")
