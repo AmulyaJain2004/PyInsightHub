32ad844fa7bd41dec000fce588ef4000c40d538f
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-18 22:21:08 +0530

    Create e_Check_character_in_String_using_membership_operator.py
    Program to check whether a charcter is present in string or not jusing membership operator without using if-else statements.

---
+++ Python_Learners_Hub/d_Basic_Input_Output /e_Check_character_in_String_using_membership_operator.py
@@ -0,0 +1,3 @@
+character = input("Enter character to check if it is present or not: ")
+string = "sdjnfsbzdfnksajnfkvjsKJwgduyqwc"
+print(f"Is the character present in string? {character in string}")
