0945251c14baf180139b64f01aab85d61cfe0bbb
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-19 23:35:13 +0530

    Update c_Nature_of_roots.py
    Program to find nature of roots of general Quadratic Equation on the basis of its coefficients.

--- Python_Learners_Hub/e_Decision_Making /c_Nature_of_roots.py
+++ Python_Learners_Hub/e_Decision_Making /c_Nature_of_roots.py
@@ -1,14 +1,16 @@
 print ("Enter the coefficients of quadratic equations according to ax^2 + bx + c =0: -")
+
 a = float(input ("Enter the value of a: "))
 b = float(input ("Enter the value of b: "))
 c = float(input ("Enter the value of c: "))
+
 d = (b**2)-(4*a*c) #discriminant
 
 if (d>0):
     print("Roots will exist and are real and distinct")
     x = (-b + d)/(2*a)
     y = (-b - d)/(2*a)
-    print("Hence, Roots are: %d %d",x,y)
+    print("Hence, Roots are: %d %d",x, y)
   
 elif (d<0):
     print("Real roots will not exist and complex roots exist")
@@ -16,5 +18,5 @@ elif (d<0):
 else: #(d==0)
     print("Roots will exist and are real and equal")
     x = (-b)/(2*a)
-    print("Hence, Equal roots are: %d %d",x,x)
+    print("Hence, Equal roots are: %d %d",x, x)
     
