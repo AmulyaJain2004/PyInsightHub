f3f68310ede04a88aab3cc1d20d71648da77c819
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-20 14:37:55 +0530

    Create c_numpy_array_add_and_subtract.py
    Proogram to perform array addition and subtraction using numpy.

---
+++ Python_Learners_Hub/j_Importing_Modules /c_importing_Numpy /c_numpy_array_add_and_subtract.py
@@ -0,0 +1,10 @@
+import numpy as np
+#universal function, vectorization, cover
+arr1 = np.array([1,2,3,4,5])
+arr2 = np.array([5,4,3,2,1])
+
+arr_result = np.add(arr1, arr2)
+print("Addition result: ", arr_result)
+
+subtract_result = np.subtract(arr1,arr2)
+print("subtraction result: ", subtract_result)
