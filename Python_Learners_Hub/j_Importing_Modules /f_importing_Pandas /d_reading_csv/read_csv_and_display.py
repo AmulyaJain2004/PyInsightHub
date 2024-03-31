bbf955739d0dac60659b889ca2acefc9fe560935
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-31 15:08:07 +0530

    Create read_csv_and_display.py
    Program to read and display already existing CSV (Comma Separated Value) files using pandas for manipulation later.

---
+++ Python_Learners_Hub/j_Importing_Modules /f_importing_Pandas /d_reading_csv/read_csv_and_display.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+file_path = 'D:/mycodes1_4_24/Python/Numpy_Matplotlib_SciPy_Pandas/pandas/tips.csv'
+df = pd.read_csv(file_path)
+
+print("DataFrame from CSV:")
+print(df)
+
+# Set the maximum number of rows to display
+pd.set_option('display.max_rows', 99)
+
+print("\nMax Rows Setting:")
+print(pd.options.display.max_rows)
