cd8f53dc3d295d0719396f0413c9a9218af9c543
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-24 16:10:42 +0530

    Update views.py

--- Python_Advanced /b_Django_Framework/FormsPractice/mymodels/myapp/views.py
+++ Python_Advanced /b_Django_Framework/FormsPractice/mymodels/myapp/views.py
@@ -6,7 +6,7 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save()  # Directly save the form to the database
             return redirect('success')  # Redirect to a success page
     else:
         form = ContactForm()
